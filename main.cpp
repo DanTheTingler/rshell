a0d8c5618e12e6f12332c3c3ca983275eb6c0258
DanTheTingler <dting001@ucr.edu>
2014-08-03 20:19:25 -0700

    added $ and user input

--- main.cpp
+++ main.cpp
@@ -1,12 +1,20 @@
 #include <iostream>
+#include <fcntl.h>
+#include <cstdlib>
+#include <stdio.h>
+#include <unistd.h>
+
 
 using namespace std;
 
-
-
 int main()
 {
+    char str[512];
 
+    cout << "$ ";
+    cin >> str;
+
+    cout << "Your command was: " << str << endl;
 
 
     return 0;
